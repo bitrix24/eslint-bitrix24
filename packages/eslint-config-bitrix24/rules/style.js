6a7246b6c8544088230137025f6b779ae0ed8b5f
placeba <compote@gmail.com>
2023-06-05 14:00:11 +0200

    do not require quotes around keywords

--- packages/eslint-config-bitrix24/rules/style.js
+++ packages/eslint-config-bitrix24/rules/style.js
@@ -441,7 +441,7 @@ module.exports = {
 		// require quotes around object literal property names
 		// https://eslint.org/docs/rules/quote-props.html
 		'quote-props': ['error', 'as-needed', {
-			keywords: true,
+			keywords: false,
 			unnecessary: true,
 			numbers: false,
 		}],
